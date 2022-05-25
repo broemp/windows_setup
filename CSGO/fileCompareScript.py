d18a6a74859831f424e580f76201ff7228fdd373
Broemp <derbrommi@live.de>
2022-05-25 22:46:07 +0200

    loop

--- CSGO/fileCompareScript.py
+++ CSGO/fileCompareScript.py
@@ -1,40 +1,49 @@
 import os
 import urllib.request
 from os.path import exists
+import sched, time
 
-REMOTE_FILE_URL="https://raw.githubusercontent.com/broemp/windows_setup/master/CSGO/autoexec.cfg"
-LOCAL_FILE_PATH="autoexec.cfg"
 
-# Load local File
-dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, LOCAL_FILE_PATH)
+def mainScript(sc):
+    REMOTE_FILE_URL="https://raw.githubusercontent.com/broemp/windows_setup/master/CSGO/autoexec.cfg"
+    LOCAL_FILE_PATH="autoexec.cfg"
 
-# Load Remote File
-remoteFile = urllib.request.urlopen(REMOTE_FILE_URL)
-remoteFileContent= remoteFile.read().decode("utf8")
+    # Load local File
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, LOCAL_FILE_PATH)
 
-#Check if file exists
-if not exists(filename):
-    print("File not found")
-    with open(filename, "w") as file:
-        file.write(remoteFileContent)
-        print("File Updated!")
-    exit()
+    # Load Remote File
+    remoteFile = urllib.request.urlopen(REMOTE_FILE_URL)
+    remoteFileContent= remoteFile.read().decode("utf8")
 
-# Load local File
-localFile=open(filename, "r")
-localFileContent=localFile.read()
-localFile.close()
+    #Check if file exists
+    if not exists(filename):
+        print("File not found")
+        with open(filename, "w") as file:
+            file.write(remoteFileContent)
+            print("File Updated!")
+        exit()
 
-# Compare Files
-if(remoteFileContent!=localFileContent):
+    # Load local File
+    localFile=open(filename, "r")
+    localFileContent=localFile.read()
+    localFile.close()
 
-    print("Unterschiede gefunden!")
-    print("Datei wird geupdatet!")
+    # Compare Files
+    if(remoteFileContent!=localFileContent):
 
-    with open(filename, "w") as file:
-        file.write(remoteFileContent)
-        print("File Updated!")
+        print("Unterschiede gefunden!")
+        print("Datei wird geupdatet!")
 
-else:
-    print("Keine Unterschiede gefunden!")
+        with open(filename, "w") as file:
+            file.write(remoteFileContent)
+            print("File Updated!")
+
+    else:
+        print("Keine Unterschiede gefunden!")
+    sc.enter(600, 1, mainScript, (sc,))
+
+s = sched.scheduler(time.time, time.sleep)
+
+s.enter(0, 1, mainScript, (s,))
+s.run()
